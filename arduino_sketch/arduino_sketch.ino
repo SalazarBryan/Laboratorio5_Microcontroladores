310a9b3857171ba0e5fb9629c6d4b9faa1697764
Isaac <isaacstalley@gmail.com>
2023-11-22 17:41:32 -0600

    Firmware working

--- arduino_sketch/arduino_sketch.ino
+++ arduino_sketch/arduino_sketch.ino
@@ -1,35 +1,36 @@
 #include <PDM.h>
 #include <Laboratorio5_inferencing.h>
 
-#define SLICES_PER_WINDW 4
+#define SLICES_PER_WINDOW 2
 #define NOISE_LABEL "NOISE"
 #define MIN_CORRECT_PREDICTION 0.8f
 
-/** Audio buffers, pointers and selectors */
+/** Audio buffers, pointers, and selectors */
 typedef struct {
     signed short *buffers[2];
     unsigned char buf_select;
     unsigned char buf_ready;
     unsigned int buf_count;
     unsigned int n_samples;
-} inference_t;
+} InferenceData;
 
-static inference_t inference;
-static bool record_ready = false;
-static signed short *sampleBuffer;
-static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
-static int print_results = -(SLICES_PER_WINDW);
+static InferenceData inference;
+static bool isRecordingReady = false;
+static signed short *audioBuffer;
+static bool enableDebug = false;
+static int printResultsCounter = -(SLICES_PER_WINDOW);
 
-static void pdm_data_ready_inference_callback(void)
+// Callback function when PDM data is ready
+static void pdmDataReadyCallback(void)
 {
     int bytesAvailable = PDM.available();
 
     // read into the sample buffer
-    int bytesRead = PDM.read((char *)&sampleBuffer[0], bytesAvailable);
+    int bytesRead = PDM.read((char *)&audioBuffer[0], bytesAvailable);
 
-    if (record_ready == true) {
-        for (int i = 0; i<bytesRead>> 1; i++) {
-            inference.buffers[inference.buf_select][inference.buf_count++] = sampleBuffer[i];
+    if (isRecordingReady == true) {
+        for (int i = 0; i < bytesRead >> 1; i++) {
+            inference.buffers[inference.buf_select][inference.buf_count++] = audioBuffer[i];
 
             if (inference.buf_count >= inference.n_samples) {
                 inference.buf_select ^= 1;
@@ -40,21 +41,22 @@ static void pdm_data_ready_inference_callback(void)
     }
 }
 
-static void microphone_inference_start(uint32_t n_samples)
+// Function to initialize the microphone for audio inference
+static void initMicrophoneInference(uint32_t numSamples)
 {
-    inference.buffers[0] = (signed short *)malloc(n_samples * sizeof(signed short));
-    inference.buffers[1] = (signed short *)malloc(n_samples * sizeof(signed short));
+    inference.buffers[0] = (signed short *)malloc(numSamples * sizeof(signed short));
+    inference.buffers[1] = (signed short *)malloc(numSamples * sizeof(signed short));
 
-    sampleBuffer = (signed short *)malloc((n_samples >> 1) * sizeof(signed short));
+    audioBuffer = (signed short *)malloc((numSamples >> 1) * sizeof(signed short));
 
     inference.buf_select = 0;
     inference.buf_count = 0;
-    inference.n_samples = n_samples;
+    inference.n_samples = numSamples;
     inference.buf_ready = 0;
 
     // configure the data receive callback
-    PDM.onReceive(&pdm_data_ready_inference_callback);
-    PDM.setBufferSize((n_samples >> 1) * sizeof(int16_t));
+    PDM.onReceive(&pdmDataReadyCallback);
+    PDM.setBufferSize((numSamples >> 1) * sizeof(int16_t));
 
     // initialize PDM with:
     // - one channel (mono mode)
@@ -64,12 +66,13 @@ static void microphone_inference_start(uint32_t n_samples)
     }
 
     // set the gain, defaults to 20
-    PDM.setGain(200);
+    PDM.setGain(20);
 
-    record_ready = true;
+    isRecordingReady = true;
 }
 
-static void microphone_inference_record(void)
+// Function to wait until the buffer is ready for recording
+static void waitForRecordingReady(void)
 {
     while (inference.buf_ready == 0) {
         delay(1);
@@ -77,29 +80,32 @@ static void microphone_inference_record(void)
     inference.buf_ready = 0;
 }
 
-static int microphone_audio_signal_get_data(size_t offset, size_t length, float *out_ptr)
+// Function to convert audio data from the inference buffer to floating-point format
+static int getAudioData(size_t offset, size_t length, float *out_ptr)
 {
     numpy::int16_to_float(&inference.buffers[inference.buf_select ^ 1][offset], out_ptr, length);
 
     return 0;
 }
 
-static void microphone_inference_end(void)
+// Function to clean up the microphone and free allocated memory
+static void endMicrophoneInference(void)
 {
     PDM.end();
     free(inference.buffers[0]);
     free(inference.buffers[1]);
-    free(sampleBuffer);
+    free(audioBuffer);
 }
 
 void setup()
 {
-    // put your setup code here, to run once:
+    // Initialize serial communication
     Serial.begin(115200);
-    // comment out the below line to cancel the wait for USB connection (needed for native USB)
+
+    // Wait for USB connection (needed for native USB)
     while (!Serial);
 
-    // summary of inferencing settings (from model_metadata.h)
+    // Print inferencing settings
     ei_printf("Inferencing settings:\n");
     ei_printf("\tInterval: %.2f ms.\n", (float)EI_CLASSIFIER_INTERVAL_MS);
     ei_printf("\tFrame size: %d\n", EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
@@ -107,38 +113,39 @@ void setup()
     ei_printf("\tNo. of classes: %d\n", sizeof(ei_classifier_inferencing_categories) /
                                             sizeof(ei_classifier_inferencing_categories[0]));
 
+    // Initialize classifier and microphone
     run_classifier_init();
-    microphone_inference_start(EI_CLASSIFIER_SLICE_SIZE);
+    initMicrophoneInference(EI_CLASSIFIER_SLICE_SIZE);
 }
 
 void loop()
 {
-    microphone_inference_record();
+    // Record audio
+    waitForRecordingReady();
 
+    // Set up the signal structure
     signal_t signal;
     signal.total_length = EI_CLASSIFIER_SLICE_SIZE;
-    signal.get_data = &microphone_audio_signal_get_data;
+    signal.get_data = &getAudioData;
     ei_impulse_result_t result = {0};
 
+    // Run continuous classification
     EI_IMPULSE_ERROR r = run_classifier_continuous(&signal, &result, false);
 
-    if (++print_results >= (SLICES_PER_WINDW)) {
-        // print the predictions
+    if (++printResultsCounter >= (SLICES_PER_WINDOW)) {
+        // Print the predictions
         ei_printf("Predictions ");
         ei_printf("(DSP: %d ms., Classification: %d ms., Anomaly: %d ms.)",
             result.timing.dsp, result.timing.classification, result.timing.anomaly);
         ei_printf(": \n");
         for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
-          if (result.classification[ix].value > 0 && result.classification[ix].label != NOISE_LABEL) {
-            ei_printf("    %s: %.5f\n", result.classification[ix].label,
-                      result.classification[ix].value);
-          }
-          if (result.classification[ix].label != NOISE_LABEL &&
-              result.classification[ix].value > MIN_CORRECT_PREDICTION) {
-            ei_printf("Detected %s: %.5f\n", result.classification[ix].label,
-                  result.classification[ix].value);
-          }
+            if (result.classification[ix].label != NOISE_LABEL &&
+                result.classification[ix].value > MIN_CORRECT_PREDICTION) {
+                ei_printf("Detected %s: %.5f\n", result.classification[ix].label,
+                          result.classification[ix].value);
+                Serial.write(result.classification[ix].label);
+            }
         }
-        print_results = 0;
+        printResultsCounter = 0;
     }
 }
